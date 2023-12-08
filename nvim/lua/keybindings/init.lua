1b38392a0ef7b79f5cf866b1f6ca861a8c97d33e
Weida Pan <weidapanx@gmail.com>
2023-12-08 15:55:11 -0800

    Set auto reload file when changed

--- nvim/lua/keybindings/init.lua
+++ nvim/lua/keybindings/init.lua
@@ -1,4 +1,9 @@
 vim.g.mapleader = ' '
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
 local map = vim.api.nvim_set_keymap
 map('n', '<C-h>', '<C-w>h', {noremap = true, silent = false})
 map('n', '<C-l>', '<C-w>l', {noremap = true, silent = false})
